be07120b6c4125f05469aac215a6df28353f6b29
Mark Bello <mcbello51286@gmail.com>
2017-11-29 15:29:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,5 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each{|badge| puts badge}
-  assign_rooms(attendees).each(|assignment| puts assignment)
+  assign_rooms(attendees).each{|assignment| puts assignment}
 end
