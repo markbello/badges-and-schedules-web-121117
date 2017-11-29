2e944e12bbfe6c846f054287b0f3021b2929b871
Mark Bello <mcbello51286@gmail.com>
2017-11-29 15:13:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def conference_badges
+  def badge_maker(arr)
+    badges = []
+    arr.each{
+      ||name||
+      badges << "Hello, my name is #{name}"
+    }
+    badges
+end
